cb6c348f2bc65cb3d7b6d4342f286e94da21b6de
nekogoro <nekogoro@yubari-exp.dev>
2020-01-19 12:48:43 +0000

    Fix script and disable some ctypes.

--- docs/title.js
+++ docs/title.js
@@ -1,10 +1,11 @@
-var list = document.getElementById("ship_list");
 var title = document.getElementById("ship_name_title");
 title.style.display = "none";
 
 function appearTitle() {
+  var list = document.ship_list;
+
   var index = list.selectedIndex;
-  if (!list.disabled && title.style.display == "none" && index > 0) {
+  if (!list.disabled && index > 0) {
     title.style.display = "block";
   } else {
     title.style.display = "none";
