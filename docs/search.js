e186482027141e00ecb22eade03a8480e746c3e2
nekogoro <nekogoro@yubari-exp.dev>
2020-01-21 19:48:48 +0000

    Prototype of search page.

--- docs/search.js
+++ docs/search.js
@@ -1,18 +1,22 @@
 $(function() {
-  var classId = '';
   var ulObj = $('#output');
   var titleObj = $('#ship-title');
 
-  var searchBox = $('#search-box');
+  var $searchBox = $('#search-box');
 
   $searchBox.on('input', function(event) {
     var input = $searchBox.val();
+    var flag = false;
     $.getJSON('search.json', function(data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].name === input) {
-          outputMaterialList(ulObj, titleObj, classId, shipId);
+          outputMaterialList(ulObj, titleObj, data[i].class, data[i].ship);
+          flag = true;
           break;
         }
+      }
+      if (!flag) {
+        console.log(input + 'is not found.');
       }
     });
   });
