6f316e904d06320d2d89a8a0a493adfb41964bcb
nekogoro <nekogoro@yubari-exp.dev>
2020-01-21 21:07:19 +0000

    Prototype of suggest.

--- docs/search.js
+++ docs/search.js
@@ -3,23 +3,23 @@ $(function() {
   var titleObj = $('#ship-title');
 
   var $searchBox = $('#search-box');
-  var $suggestObj = $('#suggest');
+  var suggestObj = $('#suggest');
 
   $searchBox.on('input', function(event) {
     var input = $searchBox.val();
-    var flag = false;
+    suggestObj.empty();
+    ulObj.empty();
+    titleObj.empty();
+    if (input === '') {
+      return;
+    }
     $.getJSON('search.json', function(data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].name.toLowerCase() === input.toLowerCase()) {
           outputMaterialList(ulObj, titleObj, data[i].name, data[i].class, data[i].ship);
-          flag = true;
         } else if (~data[i].name.toLowerCase().indexOf(input.toLowerCase())) {
-          $suggestObj.append($('<li/>').text(data[i].name));
+          suggestObj.append($('<li/>').text(data[i].name));
         }
-      }
-      if (!flag) {
-        ulObj.empty();
-        titleObj.empty();
       }
     });
   });
