d647066c4db457ba6ead01c50e9617f24b472ffb
nekogoro <nekogoro@yubari-exp.dev>
2020-01-21 21:50:27 +0000

    Prototype of suggest.

--- docs/search.js
+++ docs/search.js
@@ -4,9 +4,11 @@ $(function() {
 
   var $searchBox = $('#search-box');
   var suggestObj = $('#suggest');
+  var suggestArray = [];
 
   $searchBox.on('input', function(event) {
     var input = $searchBox.val();
+    suggestArray.empty();
     suggestObj.empty();
     ulObj.empty();
     titleObj.empty();
@@ -18,9 +20,10 @@ $(function() {
         if (data[i].name.toLowerCase() === input.toLowerCase()) {
           outputMaterialList(ulObj, titleObj, data[i].name, data[i].class, data[i].ship);
         } else if (~data[i].name.toLowerCase().indexOf(input.toLowerCase())) {
+          suggestArray.push(data[i]);
           suggestObj.append($('<li/>').text(data[i].name)
-            .on('click', function(event) {
-              outputMaterialList(ulObj, titleObj, data[i].name, data[i].class, data[i].ship);
+            .on('click', function() {
+              outputMaterialList(ulObj, titleObj, suggestArray[i].name, suggestArray[i].class, suggestArray[i].ship);
               suggestObj.empty();
             })
           );
