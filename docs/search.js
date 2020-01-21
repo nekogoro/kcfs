ab5eb504c8527ce2bc2bb1385ec9802ce65e405b
nekogoro <nekogoro@yubari-exp.dev>
2020-01-21 21:55:34 +0000

    Prototype of suggest.

--- docs/search.js
+++ docs/search.js
@@ -3,13 +3,13 @@ $(function() {
   var titleObj = $('#ship-title');
 
   var $searchBox = $('#search-box');
-  var suggestObj = $('#suggest');
+  var $suggestObj = $('#suggest');
   var suggestArray = [];
 
   $searchBox.on('input', function(event) {
     var input = $searchBox.val();
     suggestArray = [];
-    suggestObj.empty();
+    $suggestObj.empty();
     ulObj.empty();
     titleObj.empty();
     if (input === '') {
@@ -21,14 +21,16 @@ $(function() {
           outputMaterialList(ulObj, titleObj, data[i].name, data[i].class, data[i].ship);
         } else if (~data[i].name.toLowerCase().indexOf(input.toLowerCase())) {
           suggestArray.push(data[i]);
-          suggestObj.append($('<li/>').text(data[i].name)
-            .on('click', function() {
-              outputMaterialList(ulObj, titleObj, suggestArray[i].name, suggestArray[i].class, suggestArray[i].ship);
-              suggestObj.empty();
-            })
-          );
+          $suggestObj.append($('<li/>').text(data[i].name));
         }
       }
     });
   });
+  
+  $suggestObj.on('click', function() {
+    outputMaterialList(ulObj, titleObj, suggestArray[i].name, suggestArray[i].class, suggestArray[i].ship);
+    $suggestObj.empty();
+    suggestArray = [];
+  });
+
 });
