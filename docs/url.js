be4eea9f2b9b77408e9a3f9f0b15ab07a9a68cf1
nekogoro <nekogoro@yubari-exp.dev>
2021-05-16 02:52:32 +0900

    種別の即時反映

--- docs/url.js
+++ docs/url.js
@@ -14,6 +14,17 @@ $(function() {
     
     pushTypeUrl();
   })
+
+  // TODO URL で記録する
+  $('#option_label').change(function() {
+    var label = $('#option_label').prop('checked');
+  
+    if (label) {
+      $('.label_wrap').show();
+    } else {
+      $('.label_wrap').hide();
+    }
+  })
 });
 
 function pushTypeUrl() {
