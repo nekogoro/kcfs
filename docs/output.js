53026e01871e8fec1b9e5e2cac17e292f631aebe
nekogoro <nekogoro@yubari-exp.dev>
2021-05-17 22:55:11 +0900

    js 修正

--- docs/output.js
+++ docs/output.js
@@ -98,10 +98,12 @@ function checkOption() {
 }
 
 $(function() {
-  $('.list_parent input[type=checkbox]').change(function() {
+  $('.box input[type=checkbox]').change(function() {
     if ($(this).prop('checked')) {
+      console.log('checked');
       $(this).closest('li').children('ul').hide();
     } else {
+      console.log('not checked');
       $(this).closest('li').children('ul').show();
     }
     // $(this).closest('li').children('ul').toggleClass('inactive');
