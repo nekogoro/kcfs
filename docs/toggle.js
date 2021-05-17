71b7ad142940d5d591e1902663146774d81a5d96
nekogoro <nekogoro@yubari-exp.dev>
2021-05-17 23:41:46 +0900

    js 修正

--- docs/toggle.js
+++ docs/toggle.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('.parent input[type=checkbox]').change(function() {
+  $('.list_parent input[type=checkbox]').change(function() {
     if ($(this).prop('checked')) {
       $(this).closest('li').children('ul').hide();
     } else {
