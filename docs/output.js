31b6c1af356024cc02ef83f7457870a55c72745e
nekogoro <nekogoro@yubari-exp.dev>
2021-05-19 14:34:42 +0900

    class 追加テスト

--- docs/output.js
+++ docs/output.js
@@ -13,18 +13,20 @@ function outputMaterialList(ulObj, titleObj, shipTitle, classId, shipId) {
       var subObj;
       var label = generateLabelsForList(data[i].type);
       if (data[i].title.indexOf('その他') == -1) {
-        subObj = $('<li class="list_parent"/>').html(LIST_MARKOR + label + '<a href="https://akashi-list.me/#w' + data[i].id + '" title="「明石の工廠早見表」装備ページ" target="_blank" rel="noopener">' + data[i].title + '</a>'
+        subObj = $('<li class="list_parent"/>')
+          .addClass('i-' + data[i])
+          .html(LIST_MARKOR + label + '<a href="https://akashi-list.me/#w' + data[i].id + '" title="「明石の工廠早見表」装備ページ" target="_blank" rel="noopener">' + data[i].title + '</a>'
           + ' <a href="https://wikiwiki.jp/kancolle/' + data[i].title + '" title="「艦これ wiki」装備ページ" target="_blank" rel="noopener"><i class="icon icon-export"></i></a>');
       } else {
-        subObj = $('<li class="list_parent"/>').html(LIST_MARKOR + label + data[i].title);
+        subObj = $('<li class="list_parent"/>').addClass('i-' + data[i]).html(LIST_MARKOR + label + data[i].title);
       }
       var isBonusFound = false;
       for(var j = 0; j < data[i].bonus.length; j++) {
         for(var k = 0; k < data[i].bonus[j].items.length; k++) {
           if (data[i].bonus[j].items[k].ship_class == shipId) {
             subObj.append($('<ul/>')
-              .append($('<li class="list_parent"/>').append(LIST_MARKOR + data[i].bonus[j].synergy)
-                .append($('<ul/>')
+              .append($('<li class="list_parent"/>').addClass('i-' + data[i] + '-' + j).append(LIST_MARKOR + data[i].bonus[j].synergy)
+                .append($('<ul/>').addClass('i-' + data[i] + '-' + j)
                   .append($('<li>').append(data[i].bonus[j].items[k].text.replace(/(..)(-[0-9])/g, '$1<span class="bonus_minus">$2</span>'))))
                     // .replace(/(火力.[0-9]+)/g, '<span class="bonus_firepower">$1</span>')
                     // .replace(/(雷装.[0-9]+)/g, '<span class="bonus_torpedo">$1</span>')
@@ -40,8 +42,8 @@ function outputMaterialList(ulObj, titleObj, shipTitle, classId, shipId) {
             break;
           } else if (data[i].bonus[j].items[k].ship_class == classId) {
             subObj.append($('<ul/>')
-              .append($('<li class="list_parent"/>').append(LIST_MARKOR + data[i].bonus[j].synergy)
-                .append($('<ul/>')
+              .append($('<li class="list_parent"/>').addClass('i-' + data[i] + '-' + j).append(LIST_MARKOR + data[i].bonus[j].synergy)
+                .append($('<ul/>').addClass('i-' + data[i] + '-' + j)
                   .append($('<li>').append(data[i].bonus[j].items[k].text.replace(/(..)(-[0-9])/g, '$1<span class="bonus_minus">$2</span>'))))
                     // .replace(/(火力.[0-9]+)/g, '<span class="bonus_firepower">$1</span>')
                     // .replace(/(雷装.[0-9]+)/g, '<span class="bonus_torpedo">$1</span>')
